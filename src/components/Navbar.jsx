d425dbc31f5adfd0c3e7207d3387ff2a7fb3defa
AakaShVSh <97448935+AakaShVSh@users.noreply.github.com>
2024-03-04 19:15:49 +0530

    nav edit

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -24,7 +24,7 @@ const Navbar = () => {
         // color={"#5c4ce3"}
         display={"flex"}
         justifyContent={"space-between"}
-        bg="#565bf4"
+        bg="#1f4985"
         boxShadow="rgba(14, 30, 37, 0.12) 0px 2px 4px 0px, rgba(14, 30, 37, 0.32) 0px 2px 16px 0px"
 
       >
@@ -32,17 +32,17 @@ const Navbar = () => {
           boxSize={"sm"}
           w={{ base: "40%", md: "22%", lg: "13%" }}
           h={"auto"}
-          bg="#565bf4"
+          bg="#1f4985"
         >
           {" "}
-          <Image src={logo} bg={"#565bf4"} fontColor={"White"} alt="logo" />
+          <Image src={logo} bg={"#1f4985"} pb={"3%"} fontColor={"White"} alt="logo" />
         </Box>
         <Box
           as={"b"}
           
           mb={"10px"}
           w={"40%"}
-          bg="#565bf4"
+          bg="#1f4985"
           visibility={{ base: "hidden", md: "visible", lg: "visible" }}
         >
           <Input type="search"  placeholder="Search subject" w="100%" bg="#fbfbfb" />
